cb5ddef41c3f8e453316e4ac8470031d0e9b1a34
NaveenKumar3026 <naveen3026mit@gmail.com>
2026-06-22 13:14:47 +0530

    last change

--- server/index.js
+++ server/index.js
@@ -16,10 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
+  cors()
 );
 app.use(express.json());
 
